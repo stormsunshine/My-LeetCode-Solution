0c1f1481882a48697e32de26cbd594e0a85f06b5
I342834 <chenyi_storm@sina.com>
2019-12-26 08:47:57 +0800

    Create 0112. Path Sum

---
+++ Algorithms/0112. Path Sum/Solution.java
@@ -0,0 +1,19 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int sum) {
+        if (root == null)
+            return false;
+        if (root.left == null && root.right == null)
+            return root.val == sum;
+        int remain = sum - root.val;
+        return hasPathSum(root.left, remain) || hasPathSum(root.right, remain);
+    }
+}
