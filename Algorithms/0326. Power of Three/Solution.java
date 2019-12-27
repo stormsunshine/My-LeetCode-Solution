5c2897f4dd927d578039cf5ffb7b6a75fb973a3d
stormsunshine <chenyi_storm@sina.com>
2019-12-27 22:34:18 +0800

    Create 0326. Power of Three

---
+++ Algorithms/0326. Power of Three/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public boolean isPowerOfThree(int n) {
+        if (n <= 0)
+            return false;
+        while (n % 3 == 0)
+            n /= 3;
+        return n == 1;
+    }
+}
