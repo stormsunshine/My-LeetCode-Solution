af2a47b88ce128aad027929d92e5ac32908afd9d
stormsunshine <chenyi_storm@sina.com>
2019-12-29 13:40:40 +0800

    Create 1304. Find N Unique Integers Sum up to Zero

---
+++ Algorithms/1304. Find N Unique Integers Sum up to Zero/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int[] sumZero(int n) {
+        int[] array = new int[n];
+        if (n == 1)
+            array[0] = 0;
+        else {
+            int max = n / 2;
+            if (n % 2 == 0) {
+                for (int i = 1; i < n; i += 2) {
+                    array[i - 1] = -i;
+                    array[i] = i;
+                }
+            } else {
+                for (int i = 1; i < n; i += 2) {
+                    array[i] = i;
+                    array[i + 1] = -i;
+                }
+            }
+        }
+        return array;
+    }
+}
