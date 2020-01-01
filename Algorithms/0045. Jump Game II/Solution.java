a74155deb0d7072c8fd06b3ec5253eef4c87186e
stormsunshine <chenyi_storm@sina.com>
2020-01-01 10:44:33 +0800

    Create 0045. Jump Game II

---
+++ Algorithms/0045. Jump Game II/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int length = nums.length;
+        int end = 0;
+        int maxPosition = 0; 
+        int steps = 0;
+        for (int i = 0; i < length - 1; i++) {
+            maxPosition = Math.max(maxPosition, i + nums[i]); 
+            if (i == end) {
+                end = maxPosition;
+                steps++;
+            }
+        }
+        return steps;
+    }
+}
