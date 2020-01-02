4017da767bd0e4b8d5a3742fd7f0b2d510ae9d9d
I342834 <chenyi_storm@sina.com>
2020-01-02 13:52:22 +0800

    Create 0081. Search in Rotated Sorted Array II

---
+++ Algorithms/0081. Search in Rotated Sorted Array II/Solution.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean search(int[] nums, int target) {
+        int low = 0, high = nums.length - 1;
+        while (low <= high) {
+            int mid = (high - low) / 2 + low;
+            if (nums[mid] == target)
+                return true;
+            if (nums[low] == nums[mid])
+                low++;
+            else if (nums[low] < nums[mid]) {
+                if (target >= nums[low] && target <= nums[mid])
+                    high = mid - 1;
+                else
+                    low = mid + 1;
+            } else {
+                if (target <= nums[high] && target >= nums[mid])
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+        }
+        return false;
+    }
+}
