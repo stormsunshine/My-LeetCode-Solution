ff35284827d5eb110a4241703a7fdaaeeee893a6
I342834 <chenyi_storm@sina.com>
2020-01-02 10:04:42 +0800

    Create 0072. Edit Distance

---
+++ Algorithms/0072. Edit Distance/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int length1 = word1.length(), length2 = word2.length();
+        int[][] dp = new int[length1 + 1][length2 + 1];
+        for (int i = 1; i <= length1; i++)
+            dp[i][0] = i;
+        for (int i = 1; i <= length2; i++)
+            dp[0][i] = i;
+        for (int i = 1; i <= length1; i++) {
+            char c1 = word1.charAt(i - 1);
+            for (int j = 1; j <= length2; j++) {
+                char c2 = word2.charAt(j - 1);
+                if (c1 == c2)
+                    dp[i][j] = dp[i - 1][j - 1];
+                else
+                    dp[i][j] = Math.min(Math.min(dp[i - 1][j], dp[i][j - 1]), dp[i - 1][j - 1]) + 1;
+            }
+        }
+        return dp[length1][length2];
+    }
+}
