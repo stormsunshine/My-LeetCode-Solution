22efd17f5b903c1f07ee2bc4cef56bd91d017daf
stormsunshine <chenyi_storm@sina.com>
2020-01-04 12:56:31 +0800

    Create 1008. Construct Binary Search Tree from Preorder Traversal

---
+++ Algorithms/1008. Construct Binary Search Tree from Preorder Traversal/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        int length = preorder.length;
+        int[] inorder = new int[length];
+        System.arraycopy(preorder, 0, inorder, 0, length);
+        Arrays.sort(inorder);
+        return buildTree(preorder, inorder);
+    }
+
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0)
+            return null;
+        TreeNode root = new TreeNode(preorder[0]);
+        int length = preorder.length;
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        int inorderIndex = 0;
+        for (int i = 1; i < length; i++) {
+            int preorderVal = preorder[i];
+            TreeNode node = stack.peek();
+            if (node.val != inorder[inorderIndex]) {
+                node.left = new TreeNode(preorderVal);
+                stack.push(node.left);
+            } else {
+                while (!stack.isEmpty() && stack.peek().val == inorder[inorderIndex]) {
+                    node = stack.pop();
+                    inorderIndex++;
+                }
+                node.right = new TreeNode(preorderVal);
+                stack.push(node.right);
+            }
+        }
+        return root;
+    }
+}
