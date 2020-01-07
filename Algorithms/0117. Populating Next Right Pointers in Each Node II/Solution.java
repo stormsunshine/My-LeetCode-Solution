3ed1a0f72ae7618145153fbc9052c0ba57c95b08
I342834 <chenyi_storm@sina.com>
2020-01-07 18:10:40 +0800

    Create 0117. Populating Next Right Pointers in Each Node II

---
+++ Algorithms/0117. Populating Next Right Pointers in Each Node II/Solution.java
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {}
+    
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
+*/
+class Solution {
+    public Node connect(Node root) {
+        if (root == null || root.left == null && root.right == null)
+            return root;
+        Queue<Node> queue = new LinkedList<Node>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                Node node = queue.poll();
+                if (i == size - 1)
+                    node.next = null;
+                else
+                    node.next = queue.peek();
+                Node left = node.left, right = node.right;
+                if (left != null)
+                    queue.offer(left);
+                if (right != null)
+                    queue.offer(right);
+            }
+        }
+        return root;
+    }
+}
