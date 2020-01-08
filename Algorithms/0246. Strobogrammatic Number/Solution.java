d9263cfe21c39f793fc18e5addad5ed875060afa
I342834 <chenyi_storm@sina.com>
2020-01-08 12:48:28 +0800

    Create 0246. Strobogrammatic Number

---
+++ Algorithms/0246. Strobogrammatic Number/Solution.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isStrobogrammatic(String num) {
+        int length = num.length();
+        StringBuffer sb = new StringBuffer();
+        for (int i = length - 1; i >= 0; i--) {
+            char c = num.charAt(i);
+            if (c != '0' && c != '1' && c != '8' && c != '6' && c != '9')
+                return false;
+            sb.append(rotate(c));
+        }
+        String rotate = sb.toString();
+        return num.equals(rotate);
+    }
+
+    public char rotate(char c) {
+        if (c == '6' || c == '9')
+            return (char) ('6' + '9' - c);
+        else
+            return c;
+    }
+}
