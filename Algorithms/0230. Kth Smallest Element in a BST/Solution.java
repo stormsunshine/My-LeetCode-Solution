3377102546f00b6fe6cd0943d01b4a5d583c598c
stormsunshine <chenyi_storm@sina.com>
2020-01-12 19:49:29 +0800

    Create 0230. Kth Smallest Element in a BST

---
+++ Algorithms/0230. Kth Smallest Element in a BST/Solution.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public int kthSmallest(TreeNode root, int k) {
+        if (root == null)
+            return -1;
+        List<Integer> inorderTraversal = inorderTraversal(root);
+        return inorderTraversal.get(k - 1);
+    }
+
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> inorderTraversal = new ArrayList<Integer>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            TreeNode visitNode = stack.pop();
+            inorderTraversal.add(visitNode.val);
+            node = visitNode.right;
+        }
+        return inorderTraversal;
+    }
+}
