2812387297ed01fc4fc5ca2be36db6297193e80f
stormsunshine <chenyi_storm@sina.com>
2020-01-12 15:12:56 +0800

    Create 0199. Binary Tree Right Side View

---
+++ Algorithms/0199. Binary Tree Right Side View/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> viewList = new ArrayList<Integer>();
+        if (root == null)
+            return viewList;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                if (i == size - 1)
+                    viewList.add(node.val);
+                TreeNode left = node.left, right = node.right;
+                if (left != null)
+                    queue.offer(left);
+                if (right != null)
+                    queue.offer(right);
+            }
+        }
+        return viewList;
+    }
+}
