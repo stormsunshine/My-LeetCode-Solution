0e44e7174b906cd24994c11d5ac5c3e3a613b0ac
I342834 <chenyi_storm@sina.com>
2020-01-14 09:54:58 +0800

    Create 0237. Delete Node in a Linked List

---
+++ Algorithms/0237. Delete Node in a Linked List/Solution.java
@@ -0,0 +1,15 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public void deleteNode(ListNode node) {
+        ListNode nextNode = node.next;
+        node.val = nextNode.val;
+        node.next = nextNode.next;
+    }
+}
