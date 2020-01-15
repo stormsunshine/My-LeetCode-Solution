a6dd819b5a961eaa82bf00e6e181615151c77eda
I342834 <chenyi_storm@sina.com>
2020-01-15 13:12:20 +0800

    Create 0344. Reverse String

---
+++ Algorithms/0344. Reverse String/Solution.java
@@ -0,0 +1,12 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int low = 0, high = s.length - 1;
+        while (low < high) {
+            char temp = s[low];
+            s[low] = s[high];
+            s[high] = temp;
+            low++;
+            high--;
+        }
+    }
+}
