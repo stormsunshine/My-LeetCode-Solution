6b7380acd5fa6c5f9c502f880bdce15128b8c6b3
stormsunshine <chenyi_storm@sina.com>
2020-01-29 23:29:19 +0800

    Create 0516. Longest Palindromic Subsequence

---
+++ Algorithms/0516. Longest Palindromic Subsequence/Solution.java
@@ -0,0 +1,18 @@
+public class Solution {
+    public int longestPalindromeSubseq(String s) {
+        if (s == null || s.length() == 0)
+            return 0;
+        int length = s.length();
+        int[][] dp = new int[length][length];
+        for (int i = length - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < length; j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        return dp[0][length - 1];
+    }
+}
