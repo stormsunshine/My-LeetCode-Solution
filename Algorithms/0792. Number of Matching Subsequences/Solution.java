5dddcbe6ae7ea3e9dc696cf088edaa4ae426fea3
stormsunshine <chenyi_storm@sina.com>
2020-02-16 10:30:33 +0800

    Update 0792. Number of Matching Subsequences

--- Algorithms/0792. Number of Matching Subsequences/Solution.java
+++ Algorithms/0792. Number of Matching Subsequences/Solution.java
@@ -1,22 +1,35 @@
 class Solution {
     public int numMatchingSubseq(String S, String[] words) {
         int count = 0;
-        for (String word : words) {
-            if (isSubsequence(word, S))
-                count++;
+        ArrayList<Node>[] heads = new ArrayList[26];
+        for (int i = 0; i < 26; i++)
+            heads[i] = new ArrayList<Node>();
+        for (String word : words)
+            heads[word.charAt(0) - 'a'].add(new Node(word, 0));
+        char[] array = S.toCharArray();
+        for (char c : array) {
+            ArrayList<Node> oldBucket = heads[c - 'a'];
+            heads[c - 'a'] = new ArrayList<Node>();
+            for (Node node : oldBucket) {
+                node.index++;
+                int wordLength = node.word.length();
+                if (node.index == wordLength)
+                    count++;
+                else
+                    heads[node.word.charAt(node.index) - 'a'].add(node);
+            }
+            oldBucket.clear();
         }
         return count;
     }
+}
 
-    public boolean isSubsequence(String word, String str) {
-        int length1 = word.length(), length2 = str.length();
-        int index1 = 0, index2 = 0;
-        while (index1 < length1 && index2 < length2) {
-            char c1 = word.charAt(index1), c2 = str.charAt(index2);
-            if (c1 == c2)
-                index1++;
-            index2++;
-        }
-        return index1 == length1;
+class Node {
+    String word;
+    int index;
+
+    public Node(String word, int index) {
+        this.word = word;
+        this.index = index;
     }
 }
