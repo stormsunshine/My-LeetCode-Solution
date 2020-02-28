7055ee3dc4664e489cbbe5c7252c9b454b19cafd
stormsunshine <chenyi_storm@sina.com>
2020-02-28 14:23:44 +0800

    Update Solution.java

--- Algorithms/1130. Minimum Cost Tree From Leaf Values/Solution.java
+++ Algorithms/1130. Minimum Cost Tree From Leaf Values/Solution.java
@@ -21,9 +21,8 @@ class Solution {
             int start = 0, end = length - curLength;
             for (int i = start; i < end; i++) {
                 int midStart = i, midEnd = i + curLength;
-                for (int j = midStart; j < midEnd; j++) {
+                for (int j = midStart; j < midEnd; j++)
                     dp[i][i + curLength] = Math.min(dp[i][i + curLength], dp[i][j] + dp[j + 1][i + curLength] + maxValues[i][j] * maxValues[j + 1][i + curLength]);
-                }
             }
         }
         return dp[0][length - 1];
