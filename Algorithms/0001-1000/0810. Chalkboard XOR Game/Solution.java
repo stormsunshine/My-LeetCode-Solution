fcbbf86d2062dbd155f18fc23746180a5d132e2a
stormsunshine <chenyi_storm@sina.com>
2020-03-13 18:58:09 +0800

    Create 0810. Chalkboard XOR Game

---
+++ Algorithms/0001-1000/0810. Chalkboard XOR Game/Solution.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean xorGame(int[] nums) {
+        if (nums.length % 2 == 0)
+            return true;
+        int xor = 0;
+        for (int num : nums)
+            xor ^= num;
+        return xor == 0;
+    }
+}
