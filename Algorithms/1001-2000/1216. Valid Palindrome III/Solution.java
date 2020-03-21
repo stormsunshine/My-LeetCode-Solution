5e0ae57642acd13a0f84a7eb7e957823f6dffbad
stormsunshine <chenyi_storm@sina.com>
2020-03-21 18:30:57 +0800

    Create 1216. Valid Palindrome III

---
+++ Algorithms/1001-2000/1216. Valid Palindrome III/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean isValidPalindrome(String s, int k) {
+        int length = s.length();
+        int[][] dp = new int[length][length];
+        for (int i = length - 1; i >= 0; i--) {
+            dp[i][i] = 1;
+            for (int j = i + 1; j < length; j++) {
+                if (s.charAt(i) == s.charAt(j))
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                else
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+            }
+        }
+        int maxLength = dp[0][length - 1];
+        return length - maxLength <= k;
+    }
+}
