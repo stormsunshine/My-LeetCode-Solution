bcd2f55f4c61723b3514dd536ac1e69576befa92
stormsunshine <chenyi_storm@sina.com>
2020-03-24 18:03:14 +0800

    Create 1114. Print in Order

---
+++ Concurrency/1114. Print in Order/Foo.java
@@ -0,0 +1,30 @@
+class Foo {
+    private Semaphore semaphore1 = new Semaphore(1);
+    private Semaphore semaphore2 = new Semaphore(0);
+    private Semaphore semaphore3 = new Semaphore(0);
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        semaphore1.acquire();
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+        semaphore2.release();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        semaphore2.acquire();
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        semaphore3.release();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        semaphore3.acquire();
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        semaphore1.release();
+    }
+}
