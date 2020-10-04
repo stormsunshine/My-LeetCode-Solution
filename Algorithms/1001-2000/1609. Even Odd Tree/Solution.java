98061de346d03aaf9979f70b83af0d8225f3dbf4
stormsunshine <chenyi_storm@sina.com>
2020-10-04 12:39:15 +0800

    Create 1609. Even Odd Tree

---
+++ Algorithms/1001-2000/1609. Even Odd Tree/Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isEvenOddTree(TreeNode root) {
+        if (root == null)
+            return false;
+        Queue<TreeNode> queue = new LinkedList<TreeNode>();
+        queue.offer(root);
+        int level = 0;
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            int prev = level % 2 == 0 ? Integer.MIN_VALUE : Integer.MAX_VALUE;
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+                int value = node.val;
+                if (level % 2 == value % 2)
+                    return false;
+                if (level % 2 == 0 && value <= prev || level % 2 == 1 && value >= prev)
+                    return false;
+                prev = value;
+                TreeNode left = node.left, right = node.right;
+                if (left != null)
+                    queue.offer(left);
+                if (right != null)
+                    queue.offer(right);
+            }
+            level++;
+        }
+        return true;
+    }
+}
