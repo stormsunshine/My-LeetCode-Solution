e04cb97420b1c2fc52f171ccb091eaa0878cd75b
stormsunshine <chenyi_storm@sina.com>
2020-11-23 09:58:33 +0800

    Update 1665. Minimum Initial Energy to Finish Tasks

--- Algorithms/1001-2000/1665. Minimum Initial Energy to Finish Tasks/Solution.java
+++ Algorithms/1001-2000/1665. Minimum Initial Energy to Finish Tasks/Solution.java
@@ -1,14 +1,18 @@
 class Solution {
     public int minimumEffort(int[][] tasks) {
+        Arrays.sort(tasks, new Comparator<int[]>() {
+            public int compare(int[] task1, int[] task2) {
+                return (task2[1] - task2[0]) - (task1[1] - task1[0]);
+            }
+        });
+        int minEnergy = 0;
         int sum = 0;
-        int minDifference = Integer.MAX_VALUE;
-        int minRequired = 0;
-        for (int[] task : tasks) {
+        int length = tasks.length;
+        for (int i = 0; i < length; i++) {
+            int[] task = tasks[i];
+            minEnergy = Math.max(minEnergy, sum + task[1]);
             sum += task[0];
-            int difference = task[1] - task[0];
-            minDifference = Math.min(minDifference, difference);
-            minRequired = Math.max(minRequired, task[1]);
         }
-        return Math.max(sum + minDifference, minRequired);
+        return minEnergy;
     }
 }
