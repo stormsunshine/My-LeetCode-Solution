aa48d82acc02af6aa29a1f4cceaa8e80b39e5332
stormsunshine <chenyi_storm@sina.com>
2021-01-10 12:37:18 +0800

    Update 1723. Find Minimum Time to Finish All Jobs

--- Algorithms/1001-2000/1723. Find Minimum Time to Finish All Jobs/Solution.java
+++ Algorithms/1001-2000/1723. Find Minimum Time to Finish All Jobs/Solution.java
@@ -1,31 +1,29 @@
 class Solution {
-    int minTime = Integer.MAX_VALUE;
-
     public int minimumTimeRequired(int[] jobs, int k) {
-        Arrays.sort(jobs);
-        int[] times = new int[k];
         int length = jobs.length;
-        for (int i = 1; i <= k; i++)
-            times[i - 1] = jobs[length - i];
-        backtrack(jobs, k, length - k - 1, times);
-        return minTime;
-    }
-
-    public void backtrack(int[] jobs, int k, int index, int[] times) {
-        if (index < 0) {
-            int max = 0;
-            for (int i = 0; i < k; i++)
-                max = Math.max(max, times[i]);
-            minTime = Math.min(minTime, max);
-        } else {
-            int job = jobs[index];
-            for (int i = 0; i < k; i++) {
-                if (times[i] + job < minTime) {
-                    times[i] += job;
-                    backtrack(jobs, k, index - 1, times);
-                    times[i] -= job;
+        int[][] dp = new int[k + 1][1 << length];
+        for (int i = 0; i <= k; i++)
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
+        int[] times = new int[1 << length];
+        for (int mask = 0; mask < 1 << length; mask++) {
+            int time = 0;
+            for (int i = 0; i < length; i++) {
+                if ((mask & (1 << i)) != 0)
+                    time += jobs[i];
+            }
+            times[mask] = time;
+        }
+        dp[k][(1 << length) - 1] = 0;
+        for (int i = k - 1; i >= 0; i--) {
+            for (int mask = (1 << length) - 2; mask >= 0; mask--) {
+                for (int curr = 1; curr < 1 << length; curr++) {
+                    if ((curr & mask) == 0) {
+                        int time = Math.max(times[curr], dp[i + 1][curr | mask]);
+                        dp[i][mask] = Math.min(dp[i][mask], time);
+                    }
                 }
             }
         }
+        return dp[0][0];
     }
 }
